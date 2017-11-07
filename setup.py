2d7c079059103722506ad5d934b653c872d0ae1b
Weiyi Cheng <ninpy.weiyi@gmail.com>
2017-11-06 22:40:22 -0500

    define validation set

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import versioneer
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(name="iceburger",
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description=("Library for Iceburger Classification Challenge"),
+      long_description=readme(),
+      author=["Hidy Chiu, Wei-Yi Cheng"],
+      author_email=["hidy0503@gmail.com", "ninpy.weiyi@gmail.com"],
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "Programming Language :: Python :: 3.6"
+      ],
+      packages=find_packages(),
+      install_requires=[
+          "pandas",
+          "keras==2.0.9",
+          "scikit-learn>=0.18.1",
+          "h5py>=2.7.0"
+      ],
+#     extras_require={"test": ["nose", "nose-parameterized>=0.5", "mock"]},
+#     package_data={"deepsense": ["resources/templates/*",
+#                                 "resources/har_model/*"]},
+      entry_points={
+#          "console_scripts": [
+#              "swhar-data-hhar=swhar.datasets.hhar:main",
+#              "rpm-inccalc-spark=rpm.spark.inccalc:main",
+#              "rpm-luigi=rpm.pipeline:main",
+#          ],
+      },
+      include_package_data=True,
+      zip_safe=False)
