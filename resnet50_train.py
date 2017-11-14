869ad0761d96aaeb0c863c0301a5fda9ad922bde
Hidy Chiu <hidy0503@gmail.com>
2017-11-13 22:44:07 -0500

    Build model based on ResNet50 from Keras for training

---
+++ resnet50_train.py
@@ -0,0 +1,309 @@
+'''
+Training related functions and entry point
+'''
+import numpy as np
+import pandas as pd
+import cv2
+import os
+import sys
+import argparse
+import logging
+import keras
+import shutil
+
+from keras.optimizers import RMSprop, SGD
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.resnet50 import ResNet50
+from keras.layers import GlobalMaxPooling2D,Dense,BatchNormalization,GlobalAveragePooling2D,Dropout
+from keras.models import Model
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+
+from iceburger.io import parse_json_data
+
+FORMAT =  '%(asctime)-15s %(name)-8s %(levelname)s %(message)s'
+LOGNAME = 'iceburger-resnet50-train'
+
+logging.basicConfig(format=FORMAT)
+LOG = logging.getLogger(LOGNAME)
+LOG.setLevel(logging.DEBUG)
+
+PRJ = "/workspace/iceburger"
+DATA = os.path.join(PRJ, "data/processed")
+
+def get_augmentation_params(args):
+    """
+    Create augmentation parmas to feed into data generators for data augmentation
+    : param args: arguments as parsed by argparse module
+    : returns: 'dict' of augmentation parameters
+    """
+    augparams = {}
+    augparams["rotation_angle"] = args.daug_rotation
+    augparams["permutation_segments"] = args.daug_permutation_segments
+    augparams["scale"] = args.daug_scaling
+    return augparams
+
+def get_callbacks(args,model_out_path):
+    """
+    Create list of callback functions for fitting step
+    :param args: arguments as parsed by argparse module
+    :returns: `list` of `keras.callbacks` classes
+    """
+    checkpoint_name= "{mn}-best_val_loss_weights.hdf5".format(mn=args.model)
+    callbacks = []
+    #outpath = args.outpath
+
+    # save best model so far
+    callbacks.append(
+        ModelCheckpoint(
+            filepath=os.path.join(model_out_path, checkpoint_name),
+            monitor="val_loss",
+            verbose=1,
+            save_best_only=True,
+            save_weights_only=True
+        )
+    )
+    if args.cb_earlystop:
+            # stop training earlier if the model is not improving
+        callbacks.append(
+            EarlyStopping(
+                monitor="val_loss",
+                patience=args.cb_early_stop,
+                verbose=1, mode='auto'
+            )
+        )
+    if args.cb_reduce_lr:
+        callbacks.append(
+            ReduceLROnPlateau(
+                monitor="val_loss",
+                factor=args.cb_reduce_lr_factor,
+                patience=10,
+                min_lr=1e-8
+            )
+        )
+
+    return callbacks, checkpoint_name
+
+def save_model(model, fileroot):
+    """Save model architecture and weights to files
+
+    :param model: trained :class: 'keras.models.Model'
+    :param fileroot: base file root name
+    """
+    model_json = model.to_json
+    with open(fileroot+ "-iceburger.json", "w") as json_file:
+        json_file.write(model_json)
+
+    model.save_weights(fileroot + "-weights.hdf5")
+
+def compile_model(args, input_shape):
+    """Build and compile model
+
+    :param args: arguments as parsed by argparse module
+    :returns: `keras.models.Model` of compiled model
+    """
+    if args.model.lower()=="resnet50":
+        base_model = ResNet50(weights=args.weights, include_top=False,
+                              input_shape=input_shape)
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(1024, activation='relu')(x)
+        x = Dropout(0.5)(x)
+        x = Dense(512, activation='relu')(x)
+        x = Dropout(0.5)(x)
+        predictions = Dense(1, activation='sigmoid')(x)
+        model = Model(inputs=base_model.input, outputs=predictions)
+        optimizer = SGD(lr = 0.0001, momentum = 0.9)
+        #for layer in base_model.layers:
+        #    layer.trainable = False
+        for layer in model.layers[:15]:
+            layer.trainable = False
+        for layer in model.layers[15:]:
+            layer.trainable = True
+    else:
+        LOG.err("Unknown model name: {}".format(args.model))
+
+    model.compile(optimizer=optimizer,
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
+
+def train(args):
+    """Train a neural network
+
+    :param args: arguments as parsed by argparse module
+    """
+    LOG.info("Loading data from {}".format(args.data))
+    X, X_angle, y, subset = parse_json_data(os.path.join(DATA, "train_valid.json"))
+    w = 197
+    h = 197
+    X_train = np.array([cv2.resize(x,(w,h)) for x in X[subset=='train']])
+    X_angle_train = X_angle[subset=='train']
+    y_train = y[subset=='train']
+    X_valid = np.array([cv2.resize(x,(w,h)) for x in X[subset=='valid']])
+    X_angle_valid = X_angle[subset=='valid']
+    y_valid = y[subset=='valid']
+    #ds = DataSet.from_pickle(args.data)
+    #nb_classes = ds.df.activity.nunique()
+
+    LOG.info("Create generators")
+    augmentation_params = get_augmentation_params(args)
+    #input_length = pd.Timedelta(args.window_time_s, "s")
+
+    LOG.info("Initiate model")
+    model = compile_model(args, input_shape=(197,197,3))
+
+    LOG.info("Create sample generators")
+    gen_train = ImageDataGenerator(horizontal_flip = True,
+                         vertical_flip = True,
+                         width_shift_range = 0.1,
+                         height_shift_range = 0.1,
+                         zoom_range = 0.1,
+                         rotation_range = 45)
+
+    gen_valid = ImageDataGenerator(horizontal_flip = True,
+                         vertical_flip = True,
+                         width_shift_range = 0.1,
+                         height_shift_range = 0.1,
+                         zoom_range = 0.1,
+                         rotation_range = 45)
+    # Here is the function that merges our two generators
+    # We use the exact same generator with the same random seed for both the y and angle arrays
+    def gen_flow_train_for_one_input(X1, y):
+        genX1 = gen_train.flow(X1, y, batch_size= args.batch_size, seed=666)
+        while True:
+            X1i = genX1.next()
+            yield X1i[0], X1i[1]
+
+    def gen_flow_valid_for_one_input(X1, y):
+        genX1 = gen_valid.flow(X1, y, batch_size= args.batch_size, seed=444)
+        while True:
+            X1i = genX1.next()
+            yield X1i[0], X1i[1]
+    #Finally create out generator
+    gen_train = gen_flow_train_for_one_input(X_train, y_train)
+    gen_valid = gen_flow_valid_for_one_input(X_valid, y_valid)
+
+    """
+    gen_train = ds.get_generator(batch_size=args.batch_size,subset="train",
+                                 input_length=input_length,
+                                 feature_cols=feature_cols,
+                                 augmentations=augmentation_params)
+    gen_valid = ds.get_generator(batch_size=args.batch_size, subset="valid",
+                                 input_length=input_length,
+                                 feature_cols=feature_cols,
+                                 augmentations=augmentation_params)
+    """
+
+    LOG.info("Create callback functions")
+    model_out_path = os.path.join(os.path.abspath(args.outpath),"checkpoints")
+    if not os.path.exists(model_out_path):
+        os.makedirs(model_out_path)
+    callbacks, checkpoint_model_name = get_callbacks(args, model_out_path)
+
+    LOG.info("Start training ...")
+    history = model.fit_generator(
+        gen_train,
+        samples_per_epoch=args.train_steps * args.batch_size,
+        nb_epoch=args.epochs, verbose=1,
+        validation_data=gen_valid,
+        nb_val_samples=args.valid_steps * args.batch_size,
+        callbacks=callbacks)
+
+    best_idx = np.argmin(history.history['val_loss'])
+    LOG.info("Best model occurred at epoch {}:".format(best_idx))
+    LOG.info("  Best validation score: {:.4f}".format(
+        history.history["val_loss"][best_idx])
+    )
+    LOG.info("  Best validation accuracy: {:.4f} ".format(
+        history.history["val_acc"][best_idx])
+    )
+
+    best_model_name = "{mn}-best-{val_loss:.4f}-{val_acc:.4f}-weights.hdf5".format(
+        mn=args.model,
+        val_loss=history.history["val_loss"][best_idx],
+        val_acc=history.history["val_acc"][best_idx]
+    )
+    LOG.info("Rename best model to {}".format(best_model_name))
+    shutil.move(
+        os.path.join(model_out_path, checkpoint_model_name),
+        os.path.join(args.outpath, best_model_name)
+    )
+
+    LOG.info("Saving final model ...")
+    LOG.info("  Final validation score: {:.4f}".format(
+        history.history["val_loss"][-1])
+    )
+    LOG.info("  Final validation accuracy: {:.4f} ".format(
+        history.history["val_acc"][-1])
+    )
+
+    final_file_root = "{mn}-{val_loss:.4f}-{val_acc:.4f}".format(
+        mn=args.model,
+        val_loss=history.history["val_loss"][-1],
+        val_acc=history.history["val_acc"][-1]
+    )
+    model.save(os.path.join(args.outpath, final_file_root))
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "model", type=str, metavar="MODEL",
+        help="Model type for training (Options: resnet50)")
+    parser.add_argument(
+        "data", type=str, metavar="DATA",
+        help=("Path to training pickle data, as stored in"
+              " `swhar.datasets.DataSet` class"))
+    parser.add_argument(
+        "--weights", type=str, metavar="WEIGHTS",
+        help="Path to previously saved weights")
+    parser.add_argument(
+        "--batch_size", type=int, metavar="BATCH_SIZE", default=32,
+        help="Number of samples in a mini-batch")
+    parser.add_argument(
+        "--epochs", type=int, metavar="EPOCHS", default=1000,
+        help="Number of epochs")
+    parser.add_argument(
+        "--acc_only", action="store_true",
+        help="Whether to train the model using only accelerometer")
+    parser.add_argument(
+        "--train_steps", type=int, metavar="TRAIN_STEPS", default=512,
+        help=("Number of mini-batches for each epoch to pass through during"
+              " training"))
+    parser.add_argument(
+        "--valid_steps", type=int, metavar="VALID_STEPS", default=128,
+        help=("Number of mini-batches for each epoch to pass through during"
+              " validation"))
+    parser.add_argument(
+        "--daug_rotation", type=float,
+        help="Data augmentation: maximum rotation angle")
+    parser.add_argument(
+        "--daug_permutation_segments", type=int,
+        help="Data augmentation: maximum number of permutation segments")
+    parser.add_argument(
+        "--daug_scaling", type=float,
+        help="Data augmentation: maximum scaling factor")
+    parser.add_argument(
+        "--cb_early_stop", type=int, metavar="PATIENCE",
+        help="Number of epochs for early stop if without improvement")
+    parser.add_argument(
+        "--cb_reduce_lr", type=int, metavar="PLATEAU",
+        help="Number of epochs to reduce learning rate without improvement")
+    parser.add_argument(
+        "--cb_reduce_lr_factor", type=float, metavar="ALPHA",
+        help=("Factor for reducing learning rate. Only activated when"
+              " `cb_reduce_lr` is set"))
+    parser.add_argument(
+        "--outpath", type=str, metavar="OUTPATH",
+        default="./",
+        help="Output path where parsed data set class to be saved")
+    args = parser.parse_args()
+
+    model = train(args)
+
+    LOG.info("Done :)")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
